2ca4c3ada4d5f96ea955bad7993b8642e05c4829
Karan <165273760+ohiocodehunter@users.noreply.github.com>
2026-05-02 23:07:20 +0530

    Change API base URL to production server

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://extracker-backend-api-och.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
